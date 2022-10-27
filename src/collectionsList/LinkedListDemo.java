7c3f6c24e07e05f1488e33c181ec3361fc0e5d18
Rajeev Singh <rajkumar888@gmail.com>
2022-10-28 00:10:58 +0800

    27 oct class

--- src/collectionsList/LinkedListDemo.java
+++ src/collectionsList/LinkedListDemo.java
@@ -1,6 +1,7 @@
 package collectionsList;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,30 +10,86 @@ public class LinkedListDemo {
 	public static void main(String args[]) {
 
 		LinkedList<String> al = new LinkedList<String>();
+		
+		
 		System.out.println(al);
+		System.out.println(".................");
+		
 		al.add("Ravi");
 		al.add("Vijay");
-		al.add("Ravi");
-		al.add("Ajay");
+		al.add("Ravi111");
+		al.add("Ajay111");
 		al.addFirst("Ajay123");
 		al.add(null);
+		al.add("Ravi333");
+		al.addLast("Ajaylast");
 
 		System.out.println(al);
 		
-		System.out.println(al.get(0));
+//		System.out.println(al.get(0));
+//		
+//		System.out.println(al.getFirst());
+//		
+//		System.out.println(al.getLast());
 		
-		System.out.println(".................");
-		
-		List<String> linklistdemo = new LinkedList<>(
+//		System.out.println(".................");
+//		
+		LinkedList<String> linklistdemo = new LinkedList<>(
 				Arrays.asList("aa", "bbb", "cat", "dog", "Computer", "Mouse", "Keyboard", "dog", null, null));
 
 		
 		System.out.println(linklistdemo);
+		
+//		linklistdemo.addAll(al);
+		
+		linklistdemo.addAll(0,al);
+//		
+		System.out.println(".................................");
+		System.out.println(linklistdemo);
+		
+		
+//		System.out.println(linklistdemo.peek());
+		
+//		System.out.println(linklistdemo.poll());
+		System.out.println(linklistdemo.pollFirst());
+		System.out.println(".................................");
+		System.out.println(linklistdemo);
+		System.out.println(linklistdemo.pollLast());
+		System.out.println(".................................");
+		System.out.println(linklistdemo);
+		
+//		System.out.println(linklistdemo.element());
+//		System.out.println(".................................");
+//		System.out.println(linklistdemo.indexOf("Ravi333"));
+		
+		
+		
+		
+//		Iterator<String> desciter = linklistdemo.descendingIterator();
+//		
+//		while(desciter.hasNext()){
+//			
+//			System.out.print(desciter.next()+" ");
+//			
+//		}
+		
+		
+//		System.out.println(linklistdemo.contains("Ajay123"));
+//		System.out.println(linklistdemo.contains("Ajay"));
+		
+//		linklistdemo.clear();
 //		
 //		System.out.println(".................................");
+//		System.out.println(linklistdemo);
+		
 //		
+		
+//		LinkedList<String> llist2 = new LinkedList<String>(al);
 //		
-//	
+//		System.out.println(al);
+//		System.out.println(llist2);
+//		
+//		System.out.println(al.equals(llist2));	
 		
 		
 		
