17f7e2c2e30e072cec8b7309ce5f42ae730870cc
Rajeev Singh <rajkumar888@gmail.com>
2023-01-06 00:02:42 +0800

    06 jan class

--- src/actionsDemo/ActionsDemoControlClick.java
+++ src/actionsDemo/ActionsDemoControlClick.java
@@ -32,51 +32,53 @@ public class ActionsDemoControlClick {
 		
 		WebElement forgottenpasswordLink = driver.findElement(By.linkText("Forgotten password?"));
 		WebElement marketplaceLink = driver.findElement(By.linkText("Marketplace"));
-//		WebElement CareersLink = driver.findElement(By.linkText("Careers"));
+		WebElement careers = driver.findElement(By.linkText("Careers"));
 
 		actions.keyDown(Keys.CONTROL).click(forgottenpasswordLink).pause(Duration.ofSeconds(2)).click(marketplaceLink)
-				.keyUp(Keys.CONTROL).pause(Duration.ofSeconds(2)).perform();
-//
-//		Set<String> openwindowsids = driver.getWindowHandles();
-//
-//		System.out.println("count of open widnows " + openwindowsids.size());
-
-//		for (String winid : openwindowsids) {
-//
-//			
-////			System.out.println(driver.getTitle());
-////			System.out.println(driver.getCurrentUrl());
-//
-//			if (winid != mainwindowid) {
-//				driver.switchTo().window(winid);
-//				driver.close();
-//			}
-//			openwindowsids = driver.getWindowHandles();
-//		}
-
-		WebElement txtUsername = driver.findElement(By.id("email"));
-
-		actions
-		.moveToElement(txtUsername)
-		.pause(Duration.ofSeconds(2))
-		.click()
-		.pause(Duration.ofSeconds(2))
-				.keyDown(txtUsername, Keys.SHIFT)
-				.pause(Duration.ofSeconds(2))
-				.sendKeys(txtUsername, "hello world demo")
-				.pause(Duration.ofSeconds(2))
-				.keyUp(txtUsername, Keys.SHIFT)
-				.pause(Duration.ofSeconds(2))
-				.sendKeys(txtUsername, Keys.END,"world")
-				.doubleClick(txtUsername)
-				.pause(Duration.ofSeconds(2))
-				.sendKeys(Keys.BACK_SPACE)
-//				.contextClick()
-//				.pause(Duration.ofSeconds(2))
-//				.sendKeys(Keys.ESCAPE)
-//				.pause(Duration.ofSeconds(5))
-//				.build()
+				.keyUp(Keys.CONTROL).pause(Duration.ofSeconds(2))
+				.keyDown(Keys.SHIFT).click(careers).pause(Duration.ofSeconds(2))
+				.keyUp(Keys.SHIFT)
 				.perform();
+
+		Set<String> openwindowsids = driver.getWindowHandles();
+
+		System.out.println("count of open widnows " + openwindowsids.size());
+
+		for (String winid : openwindowsids) {
+			if (!winid.equals(mainwindowid)) {
+				driver.switchTo().window(winid);
+				System.out.println(driver.getTitle());
+				System.out.println(driver.getCurrentUrl());
+				driver.close();
+			}
+			System.out.println("=============================================");
+			openwindowsids = driver.getWindowHandles();
+			System.out.println("count of open widnows " + openwindowsids.size());
+		}
+
+//		WebElement txtUsername = driver.findElement(By.id("email"));
+//
+//		actions
+//		.moveToElement(txtUsername)
+//		.pause(Duration.ofSeconds(2))
+//		.click()
+//		.pause(Duration.ofSeconds(2))
+//				.keyDown(txtUsername, Keys.SHIFT)
+//				.pause(Duration.ofSeconds(2))
+//				.sendKeys(txtUsername, "hello world demo")
+//				.pause(Duration.ofSeconds(2))
+//				.keyUp(txtUsername, Keys.SHIFT)
+//				.pause(Duration.ofSeconds(2))
+//				.sendKeys(txtUsername, Keys.END,"world")
+//				.doubleClick(txtUsername)
+//				.pause(Duration.ofSeconds(2))
+//				.sendKeys(Keys.BACK_SPACE)
+////				.contextClick()
+////				.pause(Duration.ofSeconds(2))
+////				.sendKeys(Keys.ESCAPE)
+////				.pause(Duration.ofSeconds(5))
+////				.build()
+//				.perform();
 
 //		
 
